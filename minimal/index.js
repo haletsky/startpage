85fa8d55b68da6828997de27df4815982ca88e84
haletsky <bogdan1096@gmail.com>
2016-04-04 14:48:48 +0300

    Added second project

---
+++ minimal/index.js
@@ -0,0 +1,30 @@
+function initial(){
+	var templates = ['243496', 'C34447', '4F8365', '5D6299', 'ba9a6b'],
+		id = Math.floor(Math.random() * templates.length);
+
+	document.getElementsByClassName('main').item(0).style.backgroundColor = "#" + templates[id];
+	document.getElementById('image').src = templates[id] + '.jpg';
+
+	var q = document.getElementsByClassName('main').item(0);
+
+	q.style.left = window.innerWidth/2 - q.offsetWidth/2 + "px";
+	q.style.top = window.innerHeight/2 - q.offsetHeight/2 + "px";
+	
+	//Now we can show page
+	var i = 0.1;
+	var init = setInterval(function(){
+		document.body.style.opacity = i;
+		i += 0.100;
+		if(i >= 1){
+			clearInterval(init);
+		}
+	},20);
+}
+
+setInterval(function(){
+    var date = new Date();
+    var hours = date.getHours() < 10 ? '0'+date.getHours() : date.getHours();
+    var minutes = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes();
+    var seconds = date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds();
+    document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+}, 1000);
